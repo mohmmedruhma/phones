fedb9b48982d0e6bc3f79910e922e7357496b414
mohmmedruhma <75502089+mohmmedruhma@users.noreply.github.com>
2020-12-09 05:53:54 -0800

    deplying to heroku

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/phones'));
 app.get('/*',function(req,res){
-res.sendFile(path.join(__dirname+
-'/dist/phones/index.html'));});
-app.listen(process.env.PORT || 8080);
+res.sendFile(path.join(__dirname + '/dist/phones/index.html'));});
+app.listen(process.env.port || 8080);
